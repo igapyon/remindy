bf9cbd83a967e7545512a23a8fe4228a4b49be1d
igapyon <igapyon@mac.lan>
2025-08-05 11:18:35 +0900

    vernum

--- src/main/java/jp/igapyon/remindy/RemindyConstants.java
+++ src/main/java/jp/igapyon/remindy/RemindyConstants.java
@@ -1,7 +1,7 @@
 package jp.igapyon.remindy;
 
 public class RemindyConstants {
-	public static final String VERSION = "20250724a";
+	public static final String VERSION = "20250805a";
 
 	// reminders.json を外部パスに設定する場合
 	public static final String REMINDER_EXTERNAL_PATH = "";
