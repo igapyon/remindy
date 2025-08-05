ac1894929887000386fcda70c3f179dda2997f58
igapyon <igapyon@mac.lan>
2025-08-05 11:38:08 +0900

    ミリ秒を除去。

--- src/main/java/jp/igapyon/remindy/command/NotifyCommand.java
+++ src/main/java/jp/igapyon/remindy/command/NotifyCommand.java
@@ -2,6 +2,7 @@ package jp.igapyon.remindy.command;
 
 import java.awt.TrayIcon;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import jp.igapyon.remindy.core.MinuteCommand;
 import jp.igapyon.remindy.logic.MessageBuilder;
@@ -9,6 +10,7 @@ import jp.igapyon.remindy.logic.MessageBuilder;
 public class NotifyCommand implements MinuteCommand {
 	private final TrayIcon trayIcon;
 	private final MessageBuilder messageBuilder;
+	private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
 
 	public NotifyCommand(TrayIcon trayIcon, MessageBuilder messageBuilder) {
 		this.trayIcon = trayIcon;
@@ -20,7 +22,7 @@ public class NotifyCommand implements MinuteCommand {
 		String message = messageBuilder.build(now);
 		if (trayIcon != null && message != null && !message.isEmpty()) {
 			trayIcon.displayMessage("Remindy", message, TrayIcon.MessageType.INFO);
-			System.err.println("【通知】" + now + "\n" + message);
+			System.err.println("【通知】" + now.format(TIME_FORMATTER) + "\n" + message);
 		}
 	}
 }
